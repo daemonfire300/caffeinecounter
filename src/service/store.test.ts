1853dd80a95dbd54dd6901e5c2d403dc2db7efdf
Julius Foitzik <info@accountr.eu>
2021-10-07 10:15:55 +0200

    switch to branch, dont pollute master

--- src/service/store.test.ts
+++ src/service/store.test.ts
@@ -1,9 +1,28 @@
+import { Consumption, Beverage } from "@/model/beverage";
 import { StorageMock } from "./storage.mock";
 import { BeverageStore } from "./store";
 
 test('loadEmptyStore', () => {
-    let st = new BeverageStore(new StorageMock)
-    let res = st.loadStore()
-    console.log(res)
+    const st = new BeverageStore(new StorageMock)
+    const res = st.loadStore()
     expect(res).toStrictEqual(new Array<Consumption>())
+})
+
+test('storeAndAddTwoEntries', () => {
+    const st = new BeverageStore(new StorageMock)
+    const bev: Beverage = {
+        name: "Club Mate",
+        caffeine: 30,
+    }
+    const now = new Date
+    st.store(500, bev, now)
+    const res = st.loadStore()
+    const expected: Array<Consumption> = [
+        {
+            amount: 500,
+            beverage: bev,
+            date: now,
+        }
+    ]
+    expect(res).toStrictEqual(expected)
 })
--- src/service/store.ts
+++ src/service/store.ts
@@ -1,3 +1,5 @@
+import { Beverage, Consumption } from "@/model/beverage"
+
 export class BeverageStore {
 
     readonly storageKey: string = "beverageStore"
@@ -8,18 +10,32 @@ export class BeverageStore {
         this.db = db
     }
 
-    store(qty: number, ofBeverage: Beverage) {
-        //localStorage.setItem("beverageStore", JSON.stringify)
+    store(qty: number, ofBeverage: Beverage, at: Date) {
+        const c: Consumption = {
+            amount: qty,
+            beverage: ofBeverage,
+            date: at,
+        }
+        const data = this.loadStore()
+        data.push(c)
+        this.db.setItem("beverageStore", JSON.stringify(data))
     }
 
     loadStore(): Array<Consumption> {
         let store: Array<Consumption>
-        let raw: any = this.db.getItem(this.storageKey)
+        const raw: any = this.db.getItem(this.storageKey)
         if (raw == "" || raw == null) {
             store = new Array<Consumption>()
             this.setStore(store)
         } else {
             store = JSON.parse(raw)
+            store.map((value: Consumption|any): Consumption => {
+                if (value.date instanceof Date) {
+                    return value
+                }
+                value.date = new Date(value.date)
+                return value
+            })
         }
         return store
     }
