3c8cb7cffa78582228aa55b4c1dab55f3fd3473c
Julius Foitzik <info@accountr.eu>
2021-10-29 15:06:25 +0200

    refactor list into more sub-components

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,42 +1,28 @@
 <template>
   <div class="list-group">
-    <a
+    <list-item
       v-for="(val, key) in consumedBeverages"
       :key="val.date"
       :href="'#/' + key"
       :class="{ selected: visibility === key }"
       @click="visibility = key"
-      class="list-group-item list-group-item-action d-flex gap-3 py-3"
-      aria-current="true"
+      :beverage="val.beverage"
+      :amount="val.amount"
+      :date="val.date"
     >
-      <img
-        src="https://github.com/twbs.png"
-        alt="twbs"
-        width="32"
-        height="32"
-        class="rounded-circle flex-shrink-0"
-      />
-      <div class="d-flex gap-2 w-100 justify-content-between">
-        <div>
-          <h6 class="mb-0">{{ val.beverage.name }}</h6>
-          <p class="mb-0 opacity-75">
-            {{ val.amount / 100 }}l {{ val.beverage.caffeine }}mg/100ml
-            {{ (val.amount / 100) * val.beverage.caffeine }}mg
-          </p>
-        </div>
-        <small class="opacity-50 text-nowrap">{{
-          val.date.toLocaleString()
-        }}</small>
-      </div>
-    </a>
+    </list-item>
   </div>
 </template>
 
 <script lang="ts">
 import { Consumption } from "@/model/beverage";
+import ListItem from "@/components/ListItem.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
+  components: {
+    ListItem,
+  },
   props: {
     items: [],
   },
---
+++ src/components/ListItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="list-group-item list-group-item-action d-flex gap-3 py-3"
+    aria-current="true"
+  >
+    <img
+      src="https://github.com/twbs.png"
+      alt="twbs"
+      width="32"
+      height="32"
+      class="rounded-circle flex-shrink-0"
+    />
+    <div class="d-flex gap-2 w-100 justify-content-between">
+      <div class="row">
+        <h6 class="mb-0">{{ beverage.name }}</h6>
+        <p class="mb-0 opacity-75">
+          {{ amount / 100 }}l {{ beverage.caffeine }}mg/100ml
+          {{ (amount / 100) * beverage.caffeine }}mg
+        </p>
+      </div>
+      <div class="row">
+        <div class="d-flex gap-2 w-100 justify-content-between">
+          <small class="opacity-50 text-nowrap">{{
+            date.toLocaleString()
+          }}</small>
+          <span>X</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    beverage: Object,
+    amount: Number,
+    date: Object,
+  },
+});
+</script>
