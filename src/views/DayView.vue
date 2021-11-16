6b6de757bb2a66cc64b3205213f3edf45d9095fe
Julius Foitzik <info@accountr.eu>
2021-11-16 16:58:03 +0100

    i am stupid, getting things down VERY VERY slowly

--- src/views/DayView.vue
+++ src/views/DayView.vue
@@ -6,7 +6,7 @@
     </div>
     <div class="container">
       <div class="row">
-        <h1>{{ day }}</h1>
+        <h1>{{ dayDate.toLocaleDateString() }}</h1>
       </div>
       <div class="row">
         <div class="col">
@@ -27,8 +27,15 @@ import TotalCounter from "../components/TotalCounter.vue";
   props: {
     day: String,
   },
+  data() {
+    return {
+      dayDate: new Date(),
+    };
+  },
   mounted() {
-    this.$store.dispatch("loadDaily", Date.parse(this.day) ?? new Date());
+    const selectedDate: Date = new Date(parseInt(this.day)) ?? new Date();
+    this.dayDate = selectedDate;
+    this.$store.dispatch("loadDaily", selectedDate);
   },
 })
 export default class DayView extends Vue {}
