decb677ad2e0d163eeb63329dbf0df9c57a1734b
Julius Foitzik <info@accountr.eu>
2021-12-04 13:30:34 +0100

    d3js and typescript aint making no sense to me

--- src/lib/charts/chart.ts
+++ src/lib/charts/chart.ts
@@ -11,7 +11,7 @@ export function Plot(anchorNode: string, data: Array<DataPoint2D>) {
     const height = 480;
 
     let svg = d3.select(anchorNode)
-        .append('svg')
+        .append<SVGElement>('svg')
         .attr('width', width)
         .attr('height', height);
 
@@ -21,7 +21,7 @@ export function Plot(anchorNode: string, data: Array<DataPoint2D>) {
         left: 150,
         right: 30
     };
-    let plotGroup = svg.append('g')
+    let plotGroup = svg.append<SVGGElement>('g')
         .classed('plot', true)
         .attr('transform', `translate(${plotMargins.left},${plotMargins.top})`);
 
@@ -31,7 +31,7 @@ export function Plot(anchorNode: string, data: Array<DataPoint2D>) {
     let xScale = d3.scaleTime()
         .range([0, plotWidth]);
     let xAxis = d3.axisBottom(xScale);
-    let xAxisGroup = plotGroup.append('g')
+    let xAxisGroup = plotGroup.append<SVGGElement>('g')
         .classed('x', true)
         .classed('axis', true)
         .attr('transform', `translate(${0},${plotHeight})`)
@@ -40,12 +40,12 @@ export function Plot(anchorNode: string, data: Array<DataPoint2D>) {
     let yScale = d3.scaleLinear()
         .range([plotHeight, 0]);
     let yAxis = d3.axisLeft(yScale);
-    let yAxisGroup = plotGroup.append('g')
+    let yAxisGroup = plotGroup.append<SVGGElement>('g')
         .classed('y', true)
         .classed('axis', true)
         .call(yAxis);
 
-    let pointsGroup = plotGroup.append('g')
+    let pointsGroup = plotGroup.append<SVGGElement>('g')
         .classed('points', true);
 
     let extX = (d3.extent(data, d => d.xValue));
@@ -57,4 +57,22 @@ export function Plot(anchorNode: string, data: Array<DataPoint2D>) {
     yScale.domain([extY[0] ?? 0, extY[1] ?? 0])
         .nice();
     yAxisGroup.call(yAxis);
+
+    let dataBound = pointsGroup.selectAll<SVGGElement, unknown>('.post')
+        .data<DataPoint2D>(data);
+
+    // delete extra points
+    dataBound
+        .exit()
+        .remove();
+
+    // add new points
+    let enterSelection = dataBound
+        .enter()
+        .append<SVGGElement>('g')
+        .classed('post', true);
+
+    // update all existing points
+    enterSelection.merge(dataBound)
+        .attr('transform', (d) => `translate(${xScale(d.xValue)},${yScale(d.yValue)})`);
 }
